32fb094452a365fc01c77c22944111a81ba0b098
Randy Ainsworth <randyainsworth.789@gmail.com>
2025-05-15 19:13:16 -0700

    added endpoints for category and tags, admin only

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path
+from django.urls import path, include
+
+from rest_framework.routers import SimpleRouter
 from blog.views import BlogSimpleView, BlogDetailView, CommentsView, CommentDetailView, AuthorPostSerializer, \
-    AuthorDetailView, AuthorPostView
+    AuthorDetailView, AuthorPostView, CategoryViewSet, TagViewSet
+
+router = SimpleRouter()
+router.register('category', CategoryViewSet)
+router.register('tag', TagViewSet)
 
 urlpatterns = [
     path('blogs/', BlogSimpleView.as_view()),
@@ -9,4 +15,7 @@ urlpatterns = [
     path('comments/<uuid:pk>/', CommentDetailView.as_view()),
     path('profile/me/', AuthorPostView.as_view()),
     path('profile/me/<uuid:pk>/', AuthorDetailView.as_view()),
+    path('', include(router.urls)),
+
+
 ]
